97ae12865dd4186673c392b19d2002f568cbf739
Cat26 <julia.joanna.hardy@gmail.com>
2020-09-15 21:40:13 +0200

    initial commit with generated components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { ConfirmationModalComponent } from './confirmation-modal/confirmation-modal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    ConfirmationModalComponent
   ],
   imports: [
     BrowserModule
