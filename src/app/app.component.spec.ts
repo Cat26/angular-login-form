97a278f029b8470788130e33e41836eacff4d5b3
Cat26 <julia.joanna.hardy@gmail.com>
2020-09-19 20:26:17 +0200

    remove old unit test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import {LoginPageComponent} from "./login-page/login-page.component";
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -20,12 +21,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('login-form');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('login-form app is running!');
   });
 });
